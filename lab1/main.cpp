1b53b1d3b09762c7b621bbb49d1ee4d264dcb241
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2019-03-07 12:23:15 +0100

    move calculating cmax to function

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -10,12 +10,31 @@ struct rpq {
 bool q_comp(const rpq &left, const rpq &right) { return left.q > right.q; }
 bool r_comp(const rpq &left, const rpq &right) { return left.r < right.r; }
 
-void opt_alg_data13(std::vector<rpq> &rpq_vec) {
-  // Algorytm dla 1 datasetu optymalny
+// Optimal for data.1 and data.3
+void rq_sort(std::vector<rpq> &rpq_vec) {
   for (int i = 0; i < (rpq_vec.size() / 2); ++i) {
     std::sort(rpq_vec.begin() + i, rpq_vec.end() - i, r_comp);
     std::sort(rpq_vec.begin() + i + 1, rpq_vec.end() - i, q_comp);
   }
+}
+
+// Optimal for data.4
+void qr_sort(std::vector<rpq> &rpq_vec) {
+  for (int i = 0; i < (rpq_vec.size() / 2); ++i) {
+    std::sort(rpq_vec.begin()+i, rpq_vec.end()-i, q_comp);
+    std::sort(rpq_vec.begin()+i, rpq_vec.end()-i-1, r_comp);
+  }
+}
+
+int calc_cmax(std::vector<rpq> &rpq_vec) {
+  int m = rpq_vec[0].r + rpq_vec[0].p, c = m + rpq_vec[0].q;
+
+  for (int i = 1; i < rpq_vec.size(); ++i) {
+    m = rpq_vec[i].p + std::max(m, rpq_vec[i].r);
+    c = std::max(c, m + rpq_vec[i].q);
+  }
+
+  return c;
 }
 
 int main() {
@@ -36,13 +55,6 @@ int main() {
     fs >> rpq_vec[i].r >> rpq_vec[i].p >> rpq_vec[i].q;
   }
 
-  opt_alg_data13(rpq_vec);
-  int m = rpq_vec[0].r + rpq_vec[0].p, c = m + rpq_vec[0].q;
-
-  for (int i = 1; i < N; ++i) {
-    m = rpq_vec[i].p + std::max(m, rpq_vec[i].r);
-    c = std::max(c, m + rpq_vec[i].q);
-  }
-
-  std::cout << c << std::endl;
+  rq_sort(rpq_vec);
+  std::cout << calc_cmax(rpq_vec) << std::endl;
 }
