8ab515006ae27584de28da7f5838e36687659037
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2019-03-07 12:14:35 +0100

    optimize alg for data 1 and 3, remove trash

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -10,21 +10,21 @@ struct rpq {
 bool q_comp(const rpq &left, const rpq &right) { return left.q > right.q; }
 bool r_comp(const rpq &left, const rpq &right) { return left.r < right.r; }
 
-void opt_alg_data1(std::vector<rpq> &rpq_vec, int N) {
+void opt_alg_data13(std::vector<rpq> &rpq_vec) {
   // Algorytm dla 1 datasetu optymalny
-  for (int i = 0; i < (N / 4) + 1; ++i) {
+  for (int i = 0; i < (rpq_vec.size() / 2); ++i) {
     std::sort(rpq_vec.begin() + i, rpq_vec.end() - i, r_comp);
     std::sort(rpq_vec.begin() + i + 1, rpq_vec.end() - i, q_comp);
-    std::sort(rpq_vec.begin() + i + 1, rpq_vec.end() - i - 1, r_comp);
   }
 }
+
 int main() {
   std::fstream fs;
   fs.open("./rpq.data");
   int N;
   std::string str;
 
-  while (str != "data.1") {
+  while (str != "data.3") {
     fs >> str;
   }
 
@@ -36,7 +36,7 @@ int main() {
     fs >> rpq_vec[i].r >> rpq_vec[i].p >> rpq_vec[i].q;
   }
 
-  opt_alg_data1(rpq_vec, N);
+  opt_alg_data13(rpq_vec);
   int m = rpq_vec[0].r + rpq_vec[0].p, c = m + rpq_vec[0].q;
 
   for (int i = 1; i < N; ++i) {
