a63fd4c8675f2906acef69a140d97b45be203bf1
mickuc <michalxkuczaj@gmail.com>
2019-03-25 21:58:15 +0100

    kolejnosc dziala wstepnie

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -37,6 +37,7 @@ int main() {
   /* auto vec2 = read_to_vec("data.2", "witi.data"); */
   /* auto vec3 = read_to_vec("data.3", "witi.data"); */
   /* auto vec4 = read_to_vec("data.4", "witi.data"); */
+  int cmaxExported = 0;
 
   int N = 1 << vec1.size();
   std::vector<int> h_vec(N);
@@ -44,9 +45,11 @@ int main() {
   h_vec[0] = 0;
   for (int set = 1; set < N; ++set) {
     int cmax = 0;
+    cmaxExported = 0;
     for (unsigned long int i = 0, b = 1; i < vec1.size(); ++i, b *= 2)
       if (b & set)
         cmax += vec1[i].p;
+    cmaxExported = cmax;
     h_vec[set] = 999999;
     for (unsigned long int i = 0, b = 1; i < vec1.size(); ++i, b *= 2)
       if (b & set)
@@ -56,46 +59,39 @@ int main() {
   }
 
   std::cout << h_vec[N - 1] << '\n';
-  std::cout << N - 1 << '\n';
-  N -= 1; // z 100.. zrob 0111..
-  // N rowna się teraz 1111111111
-  N = N ^ (1 << 9); // zamien 10 bit na 0 (iterowane od 0 dlatego 9)
-  // N = 0111111111 czyli powinno byc 511 dziesietnie
-  std::cout << N << '\n';
-  // zgadza się
-  // teraz trzeba zrobić 1011111111 czyli 767
-  N = N ^ (1 << 9); // zamien spowrotem 10 bit
-  N = N ^ (1 << 8);
-  std::cout << N << '\n'; // printuje 767 czyli dziala;
-  N = N ^ (1 << 8);
 
-  std::cout << '\n' << "pętla co przestawia pokoleii bit na 0:\n";
-
-  // to teraz pętla żeby kazdy zamieniło z 0
-  for (int bitToReplace = 9; bitToReplace >= 0; --bitToReplace) {
-    /* if (N & 1 << bitToReplace) {
-     *   std::cout << "bit equeal 1 \n";
-     * } */
-
-    N ^= (1 << bitToReplace);
-    std::cout << N << '\n'; // printuje 767 czyli dziala;
-    /* if (!(N & 1 << bitToReplace)) {
-     *   std::cout << "bit equeal 0 \n";
-     * } */
-
-    N ^= (1 << bitToReplace);
+  N -= 1;
+  int sum = h_vec[N]; // minimalna kara ostatnia
+  int count = 0;      // jak dużo bitów w liczbie
+  int tmp = N;
+  while (tmp > 0) {
+    tmp /= 2;
+    count++;
   }
-  // trzeba odwracać spowrotem kiedy ta wartośc nie równa się naszej
-  // jeżeli się równa ten F(0111) + error naszemu optymalnemu
-  // to tego 0 na początku już nie odwracamy tylko wchodzimy
-  // do kolejnego czyli F(0111) i zmieniamy już tylko te 1
+  std::vector<int> kolejnosc;
+  for (int i = 0; i < vec1.size(); i++) {
+    for (int i = count - 1; i >= 0; --i) {
+      if (N & 1 << i) { // jezeli 0 to nie zmieniaj w binarce
+        N ^= (1 << i);
+        // std::cout << "N " << N << std::endl;
+        int kara = std::max(0, cmaxExported - vec1[i].d) * vec1[i].w;
+        int min = h_vec[N] + kara;
+        if (sum == min) {
+          // std::cout << h_vec[N] << " " << kara << " " << N
+          // << " id: " << vec1[i].id << std::endl;
+          kolejnosc.push_back(vec1[i].id);
+          cmaxExported -= vec1[i].p;
+          sum = h_vec[N];
+          break;
+        } else {
+          N ^= (1 << i);
+        }
+      }
+    }
+  }
+  std::cout << "kolejnosc:\n";
+  for (auto it = kolejnosc.end()-1; it >= kolejnosc.begin(); it--)
+    std::cout << " " << *it;
 
-  // trzeba wymyślić jak zamieniać tylko jedynki na 0 po kolei
-  // mam taki pomysł (zwykla lopata)
-  // W tej pętli sprawdzać przed zamianą bitu czy równa się 1
-  // jeżeli tak to zamień na 0 jeżeli nie to przejdz do następnego
-  // bitu
-  // temp & (1 << N)
-  // to co zakomentowane w pętli jest git i dobrze sprawdza czy bit jest jedynka
-  // GG teraz zaimplementowac
+  std::cout << std::endl;
 }
