4a880baef4f1c999fd2fc50f3cda8c0afb4fa8df
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2019-03-26 16:29:45 +0100

    hotfix

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <bitset>
 
 struct pwd {
     int p, w, d, id;
@@ -40,28 +41,30 @@ int main() {
     h_vec[0] = 0;
     for(int set=1; set<N; ++set){
         int c=0;
-        for(int i=0, b=1; i<vec1.size(); ++i, b*=2)if(b&set)
+        for(int i=0, b=1; i<vec1.size(); ++i, b*=2)if(b&set){
             c+=vec1[i].p;
+            id_vec.erase(id_vec.begin(), id_vec.end());
+        }
         h_vec[set]=999999;
-        for(int i=0, b=1; i<vec1.size(); ++i, b*=2)if(b&set)
-            h_vec[set] = std::min(h_vec.at(set), h_vec.at(set-b)+vec1.at(i).w*std::max(0, c-vec1.at(i).d)); }
+        for(int i=0, b=1; i<vec1.size(); ++i, b*=2)if(b&set){
+            h_vec[set] = std::min(h_vec.at(set),
+                    h_vec.at(set-b)+vec1.at(i).w*std::max(0, c-vec1.at(i).d));
+        }
+    }
 
-    std::cout << h_vec[N-1] << '\n';
-
-    /* int n, P[100], W[100], D[100]; */
-    /* std::ifstream data("sth.data"); */
-    /* data >> n; for(int i=0;i<n;i++) data >> P[i] >> W[i] >> D[i]; */
-
-    /* int N=1<<n, *X = new int[N]; X[0]=0; */
-    /* for(int set=1; set<N; ++set){ */
-    /*     int c=0; */
-    /*     for(int i=0, b=1; i<n; i++,b*=2)if(b&set) */
-    /*         c+=P[i]; */
-    /*     X[set]=999999; */
-    /*     for(int i=0,b=1; i<n; i++, b*=2)if(b&set) */
-    /*         X[set] = std::min(X[set], X[set-b]+W[i]*std::max(0, c-D[i])); */
+    /* for(int i=N-1; i > 0; i=~i ){ */
+    /*     std::cout << i << '\n'; */
     /* } */
-    /* std::cout << X[N-1]; */
-    /* delete[] X; */
+    // x - (x&(x-1))
+    /* std::cout << (uint)(111&(111-1)) << '\n'; */
+    /* std::cout << std::bitset<20>()(uint)(111&(111-1)) << '\n'; */
+    /* std::cout << std::bitset<11>(N) << '\n' ; */
+
+    /* std::cout << h_vec[N-1] << '\n'; */
+    /* for( auto item: id_vec ){ */
+    /*     std::cout << item << " "; */
+    /* } */
+    /*     std::cout << '\n'; */
+
     return 0;
 }
